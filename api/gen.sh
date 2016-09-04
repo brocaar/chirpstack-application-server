b5603cc5dbe94218047e841156938f56a02b022a
Orne Brocaar <info@brocaar.com>
2016-09-04 11:17:37 +0200

    Generate api and json gateway.

---
+++ api/gen.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# generate the gRPC code
+protoc -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. channel.proto channelList.proto node.proto queue.proto
+# generate the JSON interface code
+protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. channel.proto channelList.proto node.proto queue.proto
+# generate the swagger definitions
+protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --swagger_out=logtostderr=true:./swagger channel.proto channelList.proto node.proto queue.proto
+# merge the swagger code into one file
+go run swagger/main.go swagger > ../static/swagger/api.swagger.json

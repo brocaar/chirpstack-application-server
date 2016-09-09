2231083e63ab6dd0962039bb3437f8974a2f90b5
Orne Brocaar <info@brocaar.com>
2016-09-09 17:56:14 +0200

    Add test with CFList.

--- internal/api/application_server_test.go
+++ internal/api/application_server_test.go
@@ -39,22 +39,23 @@ func TestApplicationServerAPI(t *testing.T) {
 
 			So(storage.CreateNode(db, node), ShouldBeNil)
 
-			Convey("When performing a join-request", func() {
-				phy := lorawan.PHYPayload{
-					MHDR: lorawan.MHDR{
-						MType: lorawan.JoinRequest,
-						Major: lorawan.LoRaWANR1,
-					},
-					MACPayload: &lorawan.JoinRequestPayload{
-						AppEUI:   node.AppEUI,
-						DevEUI:   node.DevEUI,
-						DevNonce: [2]byte{1, 2},
-					},
-				}
-				So(phy.SetMIC(node.AppKey), ShouldBeNil)
+			phy := lorawan.PHYPayload{
+				MHDR: lorawan.MHDR{
+					MType: lorawan.JoinRequest,
+					Major: lorawan.LoRaWANR1,
+				},
+				MACPayload: &lorawan.JoinRequestPayload{
+					AppEUI:   node.AppEUI,
+					DevEUI:   node.DevEUI,
+					DevNonce: [2]byte{1, 2},
+				},
+			}
+			So(phy.SetMIC(node.AppKey), ShouldBeNil)
 
-				b, err := phy.MarshalBinary()
-				So(err, ShouldBeNil)
+			b, err := phy.MarshalBinary()
+			So(err, ShouldBeNil)
+
+			Convey("When performing a join-request", func() {
 
 				req := as.JoinRequestRequest{
 					PhyPayload: b,
@@ -99,6 +100,66 @@ func TestApplicationServerAPI(t *testing.T) {
 
 					Convey("Then the DevAddr of the node has been updated", func() {
 						So(node.DevAddr[:], ShouldResemble, []byte{1, 2, 3, 4})
+					})
+				})
+			})
+
+			Convey("Given the node as a CFList with three channels", func() {
+				cl := storage.ChannelList{
+					Name: "test list",
+				}
+				So(storage.CreateChannelList(db, &cl), ShouldBeNil)
+				So(storage.CreateChannel(db, &storage.Channel{
+					ChannelListID: cl.ID,
+					Channel:       3,
+					Frequency:     868400000,
+				}), ShouldBeNil)
+				So(storage.CreateChannel(db, &storage.Channel{
+					ChannelListID: cl.ID,
+					Channel:       4,
+					Frequency:     868500000,
+				}), ShouldBeNil)
+				So(storage.CreateChannel(db, &storage.Channel{
+					ChannelListID: cl.ID,
+					Channel:       5,
+					Frequency:     868600000,
+				}), ShouldBeNil)
+
+				node.ChannelListID = &cl.ID
+				So(storage.UpdateNode(db, node), ShouldBeNil)
+
+				Convey("When performing a join-request", func() {
+					req := as.JoinRequestRequest{
+						PhyPayload: b,
+						DevAddr:    []byte{1, 2, 3, 4},
+						NetID:      []byte{1, 2, 3},
+					}
+
+					resp, err := api.JoinRequest(ctx, &req)
+					So(err, ShouldBeNil)
+
+					Convey("Then the CFlist is set in the response", func() {
+						node, err := storage.GetNode(db, node.DevEUI)
+						So(err, ShouldBeNil)
+
+						var phy lorawan.PHYPayload
+						So(phy.UnmarshalBinary(resp.PhyPayload), ShouldBeNil)
+
+						So(phy.DecryptJoinAcceptPayload(node.AppKey), ShouldBeNil)
+						ok, err := phy.ValidateMIC(node.AppKey)
+						So(err, ShouldBeNil)
+						So(ok, ShouldBeTrue)
+
+						jaPL, ok := phy.MACPayload.(*lorawan.JoinAcceptPayload)
+						So(ok, ShouldBeTrue)
+
+						So(jaPL.CFList, ShouldResemble, &lorawan.CFList{
+							868400000,
+							868500000,
+							868600000,
+							0,
+							0,
+						})
 					})
 				})
 			})

8af5844a0635dc311f553f606cbc03946cd3d0c0
Orne Brocaar <info@brocaar.com>
2016-09-24 21:00:31 +0200

    Update links.

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -6,13 +6,13 @@ class Navbar extends Component {
       <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="#">LoRa Server UI</a>
+            <a className="navbar-brand" href="#">LoRa Server</a>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav navbar-right">
               <li><a href="#">Set JWT token</a></li>
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Documentation</a></li>
+              <li><a href="https://github.com/brocaar/loraserver">GitHub</a></li>
+              <li><a href="https://docs.loraserver.io/">Documentation</a></li>
             </ul>
           </div>
         </div>
@@ -22,4 +22,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-

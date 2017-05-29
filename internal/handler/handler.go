adda1a4a9933a9d45cb393337b29a0b821fb7827
Orne Brocaar <info@brocaar.com>
2017-05-29 10:43:27 +0200

    Implement HTTPHandler.

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,10 +2,15 @@ package handler
 
 // Handler defines the interface of a handler backend.
 type Handler interface {
-	Close() error                                          // closes the handler
+	IntegrationHandler
+	DataDownChan() chan DataDownPayload // returns DataDownPayload channel
+}
+
+// IntegrationHandler defines the interface of an integration handler.
+type IntegrationHandler interface {
 	SendDataUp(payload DataUpPayload) error                // send data-up payload
 	SendJoinNotification(payload JoinNotification) error   // send join notification
 	SendACKNotification(payload ACKNotification) error     // send ack notification
 	SendErrorNotification(payload ErrorNotification) error // send error notification
-	DataDownChan() chan DataDownPayload                    // returns DataDownPayload channel
+	Close() error                                          // closes the handler
 }

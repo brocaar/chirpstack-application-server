0a7af52080e929ae2cd9bea19a5d127522b2ab79
Orne Brocaar <info@brocaar.com>
2017-05-30 11:09:12 +0200

    Remove unused import.

--- ui/src/views/applications/CreateApplication.js
+++ ui/src/views/applications/CreateApplication.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from 'react-router';
 
 import ApplicationStore from "../../stores/ApplicationStore";
 import ApplicationForm from "../../components/ApplicationForm";

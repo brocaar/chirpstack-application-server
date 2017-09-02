d83fd9b2a11fef653bb21071db8af92ebff11348
Orne Brocaar <info@brocaar.com>
2017-09-02 12:39:20 +0200

    Add UI component for HTTP headers.

--- ui/src/components/ApplicationIntegrationForm.js
+++ ui/src/components/ApplicationIntegrationForm.js
@@ -3,10 +3,51 @@ import React, { Component } from 'react';
 import Select from "react-select";
 
 
+class ApplicationHTTPIntegrationHeaderForm extends Component {
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  onChange(field, e) {
+    let header = this.props.header;
+    header[field] = e.target.value;
+
+    this.props.onHeaderChange(this.props.index, header);
+  }
+
+  onDelete(e) {
+    this.props.onDeleteHeader(this.props.index);
+    e.preventDefault();
+  }
+
+  render() {
+    return(
+      <div className="form-group row">
+        <div className="col-sm-4">
+          <input type="text" className="form-control" placeholder="Header name" value={this.props.header.key || ''} onChange={this.onChange.bind(this, 'key')} />
+        </div>
+        <div className="col-sm-7">
+          <input type="text" className="form-control" placeholder="Header value" value={this.props.header.value || ''} onChange={this.onChange.bind(this, 'value')} />
+        </div>
+        <div className="col-sm-1">
+          <button type="button" className="btn btn-link pull-right" onClick={this.onDelete}>
+            <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
 class ApplicationHTTPIntegrationForm extends Component {
   constructor() {
     super();
     this.onChange = this.onChange.bind(this);
+    this.onHeaderChange = this.onHeaderChange.bind(this);
+    this.addHeader = this.addHeader.bind(this);
+    this.onDeleteHeader = this.onDeleteHeader.bind(this);
   }
 
   onChange(field, e) {
@@ -16,25 +57,69 @@ class ApplicationHTTPIntegrationForm extends Component {
     this.props.onFormChange(integration);
   }
 
+  addHeader(e) {
+    let integration = this.props.integration;
+    if (typeof(integration.headers) === "undefined") {
+      integration.headers = [{}];
+    } else {
+      integration.headers.push({});
+    }
+
+    this.props.onFormChange(integration);
+
+    e.preventDefault();
+  }
+
+  onHeaderChange(index, header) {
+    let integration = this.props.integration;
+    integration.headers[index] = header;
+
+    this.props.onFormChange(integration);
+  }
+
+  onDeleteHeader(index) {
+    let integration = this.props.integration;
+    integration.headers.splice(index, 1);
+
+    this.props.onFormChange(integration);
+  }
+
   render() {
+    let headers = [];
+    if (typeof(this.props.integration.headers) !== "undefined") {
+      headers = this.props.integration.headers;
+    }
+
+    const HTTPHeaders = headers.map((header, i) => <ApplicationHTTPIntegrationHeaderForm key={i} index={i} header={header} onHeaderChange={this.onHeaderChange} onDeleteHeader={this.onDeleteHeader} />);
+
     return(
       <div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="dataUpURL">Uplink data URL</label>
-          <input className="form-control" id="dataUpURL" name="dataUpURL" type="text" placeholder="http://example.com/uplink" value={this.props.integration.dataUpURL || ''} onChange={this.onChange.bind(this, 'dataUpURL')} />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="joinNotificationURL">Join notification URL</label>
-          <input className="form-control" id="joinNotificationURL" name="joinNotificationURL" type="text" placeholder="http://example.com/join" value={this.props.integration.joinNotificationURL || ''} onChange={this.onChange.bind(this, 'joinNotificationURL')} />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="ackNotificationURL">ACK notification URL</label>
-          <input className="form-control" id="ackNotificationURL" name="ackNotificationURL" type="text" placeholder="http://example.com/ack" value={this.props.integration.ackNotificationURL || ''} onChange={this.onChange.bind(this, 'ackNotificationURL')} />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="errorNotificationURL">Error notification URL</label>
-          <input className="form-control" id="errorNotificationURL" name="errorNotificationURL" type="text" placeholder="http://example.com/error" value={this.props.integration.errorNotificationURL || ''} onChange={this.onChange.bind(this, 'errorNotificationURL')} />
-        </div>
+        <fieldset>
+          <legend>Headers</legend>
+          {HTTPHeaders}
+          <div className="form-group">
+            <button className="btn btn-default pull-right" onClick={this.addHeader}>Add header</button>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Endpoints</legend>
+          <div className="form-group">
+            <label className="control-label" htmlFor="dataUpURL">Uplink data URL</label>
+            <input className="form-control" id="dataUpURL" name="dataUpURL" type="text" placeholder="http://example.com/uplink" value={this.props.integration.dataUpURL || ''} onChange={this.onChange.bind(this, 'dataUpURL')} />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="joinNotificationURL">Join notification URL</label>
+            <input className="form-control" id="joinNotificationURL" name="joinNotificationURL" type="text" placeholder="http://example.com/join" value={this.props.integration.joinNotificationURL || ''} onChange={this.onChange.bind(this, 'joinNotificationURL')} />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="ackNotificationURL">ACK notification URL</label>
+            <input className="form-control" id="ackNotificationURL" name="ackNotificationURL" type="text" placeholder="http://example.com/ack" value={this.props.integration.ackNotificationURL || ''} onChange={this.onChange.bind(this, 'ackNotificationURL')} />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="errorNotificationURL">Error notification URL</label>
+            <input className="form-control" id="errorNotificationURL" name="errorNotificationURL" type="text" placeholder="http://example.com/error" value={this.props.integration.errorNotificationURL || ''} onChange={this.onChange.bind(this, 'errorNotificationURL')} />
+          </div>
+        </fieldset>
       </div>
     );
   }

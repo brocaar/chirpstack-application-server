f122d5b0a4760759fb92120d239c8328719daeaa
Orne Brocaar <info@brocaar.com>
2017-10-17 10:06:50 +0200

    Rename migration. Move search form to panel heading.

--- ui/src/views/nodes/ListNodes.js
+++ ui/src/views/nodes/ListNodes.js
@@ -82,27 +82,36 @@ class ListNodes extends Component {
     
   }
 
-  searchNodes() {
+  searchNodes(e) {
+    e.preventDefault();
     this.updatePage(this.props);
   }
 
   render() {
     const NodeRows = this.state.nodes.map((node, i) => <NodeRow key={node.devEUI} node={node} application={this.state.application} />);
 
+    const searchStyle = {
+      width: "200px",
+    };
+
     return(
       <div className="panel panel-default">
-        <div className={`panel-heading clearfix ${this.state.isAdmin ? '' : 'hidden'}`}>
-          <div className="btn-group pull-right">
+        <div className="panel-heading clearfix">
+          <form className="form-inline pull-left" onSubmit={this.searchNodes}>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon">
+                  <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </div>
+                <input type="text" className="form-control" style={searchStyle} placeholder="Device name or DevEUI" onChange={this.onChange} value={this.state.search || ''} />
+              </div>
+            </div>
+          </form>
+          <div className={`btn-group pull-right ${this.state.isAdmin ? "" : "hidden"}`}>
             <Link to={`/organizations/${this.props.params.organizationID}/applications/${this.state.application.id}/nodes/create`}><button type="button" className="btn btn-default btn-sm">Create node</button></Link>
           </div>
         </div>
         <div className="panel-body">
-          <div className="input-group col-md-3">
-            <input type="text" className="form-control" placeholder="Search for..." onChange={this.onChange} value={this.state.search || ''} />
-            <span className="input-group-btn">
-              <button className="btn btn-default" onClick={this.searchNodes} type="button"><span className="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-            </span>
-          </div>
           <table className="table table-hover">
             <thead>
               <tr>

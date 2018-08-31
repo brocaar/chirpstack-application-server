52458255c7a5783746dec7a7de28adce95f406be
Orne Brocaar <info@brocaar.com>
2018-08-31 13:16:20 +0200

    Remove service-worker.
    This fixes the issue where /api would be served sometimes when the the
    request would be catched by the service-worker.

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,6 @@ import "typeface-roboto";
 import Leaflet from "leaflet";
 
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 import "leaflet/dist/leaflet.css";
 import "codemirror/lib/codemirror.css";
@@ -15,4 +14,3 @@ import "./index.css";
 Leaflet.Icon.Default.imagePath = "//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/"
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();

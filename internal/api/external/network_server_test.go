ce80c6aebf4f175a0cac4f539a816ea8808e7861
Orne Brocaar <info@brocaar.com>
2019-06-07 13:58:59 +0200

    Refactor network-server API tests.

--- internal/api/external/network_server_test.go
+++ internal/api/external/network_server_test.go
@@ -3,6 +3,9 @@ package external
 import (
 	"testing"
 
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 
@@ -10,191 +13,207 @@ import (
 	"github.com/brocaar/lora-app-server/internal/backend/networkserver"
 	"github.com/brocaar/lora-app-server/internal/backend/networkserver/mock"
 	"github.com/brocaar/lora-app-server/internal/storage"
-	"github.com/brocaar/lora-app-server/internal/test"
-	. "github.com/smartystreets/goconvey/convey"
-	"golang.org/x/net/context"
 )
 
-func TestNetworkServerAPI(t *testing.T) {
-	conf := test.GetConfig()
-	if err := storage.Setup(conf); err != nil {
-		t.Fatal(err)
+func (ts *APITestSuite) TestNetworkServer() {
+	assert := require.New(ts.T())
+
+	nsClient := mock.NewClient()
+	networkserver.SetPool(mock.NewPool(nsClient))
+
+	validator := &TestValidator{}
+	api := NewNetworkServerAPI(validator)
+
+	org := storage.Organization{
+		Name: "test-org",
 	}
+	assert.NoError(storage.CreateOrganization(storage.DB(), &org))
 
-	Convey("Given a clean database and api instance", t, func() {
-		test.MustResetDB(storage.DB().DB)
+	ts.T().Run("Create", func(t *testing.T) {
+		assert := require.New(t)
 
-		nsClient := mock.NewClient()
-		networkserver.SetPool(mock.NewPool(nsClient))
-
-		ctx := context.Background()
-		validator := &TestValidator{}
-		api := NewNetworkServerAPI(validator)
-
-		org := storage.Organization{
-			Name: "test-org",
+		createReq := pb.CreateNetworkServerRequest{
+			NetworkServer: &pb.NetworkServer{
+				Name:                        "test ns",
+				Server:                      "test-ns:1234",
+				CaCert:                      "CACERT",
+				TlsCert:                     "TLSCERT",
+				TlsKey:                      "TLSKEY",
+				RoutingProfileCaCert:        "RPCACERT",
+				RoutingProfileTlsCert:       "RPTLSCERT",
+				RoutingProfileTlsKey:        "RPTLSKEY",
+				GatewayDiscoveryEnabled:     true,
+				GatewayDiscoveryInterval:    5,
+				GatewayDiscoveryTxFrequency: 868100000,
+				GatewayDiscoveryDr:          5,
+			},
 		}
-		So(storage.CreateOrganization(storage.DB(), &org), ShouldBeNil)
 
-		Convey("Then Create creates a network-server", func() {
-			createReq := pb.CreateNetworkServerRequest{
+		resp, err := api.Create(context.Background(), &createReq)
+		assert.NoError(err)
+		assert.True(resp.Id > 0)
+
+		t.Run("Get", func(t *testing.T) {
+			assert := require.New(t)
+
+			getResp, err := api.Get(context.Background(), &pb.GetNetworkServerRequest{
+				Id: resp.Id,
+			})
+			assert.NoError(err)
+
+			createReq.NetworkServer.Id = resp.Id
+			createReq.NetworkServer.TlsKey = "" // key is not returned on get
+			createReq.NetworkServer.RoutingProfileTlsKey = ""
+
+			assert.Equal(createReq.NetworkServer, getResp.NetworkServer)
+		})
+
+		t.Run("CA and TLS fields are populated", func(t *testing.T) {
+			assert := require.New(t)
+
+			n, err := storage.GetNetworkServer(storage.DB(), resp.Id)
+			assert.NoError(err)
+
+			assert.Equal("CACERT", n.CACert)
+			assert.Equal("TLSCERT", n.TLSCert)
+			assert.Equal("TLSKEY", n.TLSKey)
+			assert.Equal("RPCACERT", n.RoutingProfileCACert)
+			assert.Equal("RPTLSCERT", n.RoutingProfileTLSCert)
+			assert.Equal("RPTLSKEY", n.RoutingProfileTLSKey)
+		})
+
+		t.Run("List as non-admin", func(t *testing.T) {
+			assert := require.New(t)
+
+			listResp, err := api.List(context.Background(), &pb.ListNetworkServerRequest{
+				Limit:  10,
+				Offset: 0,
+			})
+			assert.NoError(err)
+
+			assert.EqualValues(0, listResp.TotalCount, 0)
+			assert.Len(listResp.Result, 0)
+		})
+
+		t.Run("List as admin", func(t *testing.T) {
+			assert := require.New(t)
+			validator.returnIsAdmin = true
+
+			listResp, err := api.List(context.Background(), &pb.ListNetworkServerRequest{
+				Limit:  10,
+				Offset: 0,
+			})
+			assert.NoError(err)
+
+			assert.EqualValues(1, listResp.TotalCount)
+			assert.Len(listResp.Result, 1)
+			assert.Equal("test ns", listResp.Result[0].Name)
+			assert.Equal("test-ns:1234", listResp.Result[0].Server)
+		})
+
+		t.Run("Second organization and service-profile assigned to the first organization", func(t *testing.T) {
+			assert := require.New(t)
+
+			org2 := storage.Organization{
+				Name: "test-org-2",
+			}
+			assert.NoError(storage.CreateOrganization(storage.DB(), &org2))
+
+			sp := storage.ServiceProfile{
+				NetworkServerID: resp.Id,
+				OrganizationID:  org.ID,
+				Name:            "test-sp",
+			}
+			assert.NoError(storage.CreateServiceProfile(storage.DB(), &sp))
+
+			t.Run("List with organization id filter", func(t *testing.T) {
+				assert := require.New(t)
+
+				listResp, err := api.List(context.Background(), &pb.ListNetworkServerRequest{
+					Limit:          10,
+					OrganizationId: org.ID,
+				})
+				assert.NoError(err)
+
+				assert.EqualValues(1, listResp.TotalCount)
+				assert.Len(listResp.Result, 1)
+
+				listResp, err = api.List(context.Background(), &pb.ListNetworkServerRequest{
+					Limit:          10,
+					OrganizationId: org2.ID,
+				})
+				assert.NoError(err)
+
+				assert.EqualValues(0, listResp.TotalCount)
+				assert.Len(listResp.Result, 0)
+			})
+
+			assert.NoError(storage.DeleteOrganization(storage.DB(), org2.ID))
+			var spID uuid.UUID
+			copy(spID[:], sp.ServiceProfile.Id)
+			assert.NoError(storage.DeleteServiceProfile(storage.DB(), spID))
+		})
+
+		t.Run("Update", func(t *testing.T) {
+			assert := require.New(t)
+
+			updateReq := pb.UpdateNetworkServerRequest{
 				NetworkServer: &pb.NetworkServer{
-					Name:                        "test ns",
-					Server:                      "test-ns:1234",
-					CaCert:                      "CACERT",
-					TlsCert:                     "TLSCERT",
-					TlsKey:                      "TLSKEY",
-					RoutingProfileCaCert:        "RPCACERT",
-					RoutingProfileTlsCert:       "RPTLSCERT",
-					RoutingProfileTlsKey:        "RPTLSKEY",
-					GatewayDiscoveryEnabled:     true,
-					GatewayDiscoveryInterval:    5,
-					GatewayDiscoveryTxFrequency: 868100000,
-					GatewayDiscoveryDr:          5,
+					Id:                          resp.Id,
+					Name:                        "updated-test-ns",
+					Server:                      "updated-test-ns:1234",
+					CaCert:                      "CACERT2",
+					TlsCert:                     "TLSCERT2",
+					TlsKey:                      "TLSKEY2",
+					RoutingProfileCaCert:        "RPCACERT2",
+					RoutingProfileTlsCert:       "RPTLSCERT2",
+					RoutingProfileTlsKey:        "RPTLSKEY2",
+					GatewayDiscoveryEnabled:     false,
+					GatewayDiscoveryInterval:    1,
+					GatewayDiscoveryTxFrequency: 868300000,
+					GatewayDiscoveryDr:          4,
 				},
 			}
 
-			resp, err := api.Create(ctx, &createReq)
-			So(err, ShouldBeNil)
-			So(resp.Id, ShouldBeGreaterThan, 0)
+			_, err := api.Update(context.Background(), &updateReq)
+			assert.NoError(err)
 
-			Convey("Then Get returns the network-server", func() {
-				getResp, err := api.Get(ctx, &pb.GetNetworkServerRequest{
-					Id: resp.Id,
-				})
-				So(err, ShouldBeNil)
-
-				createReq.NetworkServer.Id = resp.Id
-				createReq.NetworkServer.TlsKey = "" // key is not returned on get
-				createReq.NetworkServer.RoutingProfileTlsKey = ""
-
-				So(getResp.NetworkServer, ShouldResemble, createReq.NetworkServer)
+			getResp, err := api.Get(context.Background(), &pb.GetNetworkServerRequest{
+				Id: resp.Id,
 			})
+			assert.NoError(err)
 
-			Convey("Then the CA and TLS fields are populated", func() {
-				n, err := storage.GetNetworkServer(storage.DB(), resp.Id)
-				So(err, ShouldBeNil)
-				So(n.CACert, ShouldEqual, "CACERT")
-				So(n.TLSCert, ShouldEqual, "TLSCERT")
-				So(n.TLSKey, ShouldEqual, "TLSKEY")
-				So(n.RoutingProfileCACert, ShouldEqual, "RPCACERT")
-				So(n.RoutingProfileTLSCert, ShouldEqual, "RPTLSCERT")
-				So(n.RoutingProfileTLSKey, ShouldEqual, "RPTLSKEY")
+			updateReq.NetworkServer.TlsKey = "" // is not returned on get
+			updateReq.NetworkServer.RoutingProfileTlsKey = ""
+			assert.Equal(updateReq.NetworkServer, getResp.NetworkServer)
+
+			n, err := storage.GetNetworkServer(storage.DB(), resp.Id)
+			assert.NoError(err)
+
+			assert.Equal("CACERT2", n.CACert)
+			assert.Equal("TLSCERT2", n.TLSCert)
+			assert.Equal("TLSKEY2", n.TLSKey)
+			assert.Equal("RPCACERT2", n.RoutingProfileCACert)
+			assert.Equal("RPTLSCERT2", n.RoutingProfileTLSCert)
+			assert.Equal("RPTLSKEY2", n.RoutingProfileTLSKey)
+			assert.False(n.GatewayDiscoveryEnabled)
+			assert.Equal(1, n.GatewayDiscoveryInterval)
+			assert.Equal(868300000, n.GatewayDiscoveryTXFrequency)
+			assert.Equal(4, n.GatewayDiscoveryDR)
+		})
+
+		t.Run("Delete", func(t *testing.T) {
+			assert := require.New(t)
+
+			_, err := api.Delete(context.Background(), &pb.DeleteNetworkServerRequest{
+				Id: resp.Id,
 			})
+			assert.NoError(err)
 
-			Convey("Then Update updates the network-server", func() {
-				updateReq := pb.UpdateNetworkServerRequest{
-					NetworkServer: &pb.NetworkServer{
-						Id:                          resp.Id,
-						Name:                        "updated-test-ns",
-						Server:                      "updated-test-ns:1234",
-						CaCert:                      "CACERT2",
-						TlsCert:                     "TLSCERT2",
-						TlsKey:                      "TLSKEY2",
-						RoutingProfileCaCert:        "RPCACERT2",
-						RoutingProfileTlsCert:       "RPTLSCERT2",
-						RoutingProfileTlsKey:        "RPTLSKEY2",
-						GatewayDiscoveryEnabled:     false,
-						GatewayDiscoveryInterval:    1,
-						GatewayDiscoveryTxFrequency: 868300000,
-						GatewayDiscoveryDr:          4,
-					},
-				}
-
-				_, err := api.Update(ctx, &updateReq)
-				So(err, ShouldBeNil)
-
-				getResp, err := api.Get(ctx, &pb.GetNetworkServerRequest{
-					Id: resp.Id,
-				})
-				So(err, ShouldBeNil)
-
-				updateReq.NetworkServer.TlsKey = "" // is not returned on get
-				updateReq.NetworkServer.RoutingProfileTlsKey = ""
-				So(getResp.NetworkServer, ShouldResemble, updateReq.NetworkServer)
-
-				Convey("Then the network-server is updated", func() {
-					n, err := storage.GetNetworkServer(storage.DB(), resp.Id)
-					So(err, ShouldBeNil)
-					So(n.CACert, ShouldEqual, "CACERT2")
-					So(n.TLSCert, ShouldEqual, "TLSCERT2")
-					So(n.TLSKey, ShouldEqual, "TLSKEY2")
-					So(n.RoutingProfileCACert, ShouldEqual, "RPCACERT2")
-					So(n.RoutingProfileTLSCert, ShouldEqual, "RPTLSCERT2")
-					So(n.RoutingProfileTLSKey, ShouldEqual, "RPTLSKEY2")
-					So(n.GatewayDiscoveryEnabled, ShouldBeFalse)
-					So(n.GatewayDiscoveryInterval, ShouldEqual, 1)
-					So(n.GatewayDiscoveryTXFrequency, ShouldEqual, 868300000)
-					So(n.GatewayDiscoveryDR, ShouldEqual, 4)
-				})
+			_, err = api.Get(context.Background(), &pb.GetNetworkServerRequest{
+				Id: resp.Id,
 			})
-
-			Convey("Then Delete deletes the network-server", func() {
-				_, err := api.Delete(ctx, &pb.DeleteNetworkServerRequest{
-					Id: resp.Id,
-				})
-				So(err, ShouldBeNil)
-
-				_, err = api.Get(ctx, &pb.GetNetworkServerRequest{
-					Id: resp.Id,
-				})
-				So(err, ShouldNotBeNil)
-				So(grpc.Code(err), ShouldEqual, codes.NotFound)
-			})
-
-			Convey("Then List lists the network-servers", func() {
-				// non admin returns nothing
-				listResp, err := api.List(ctx, &pb.ListNetworkServerRequest{
-					Limit:  10,
-					Offset: 0,
-				})
-				So(err, ShouldBeNil)
-				So(listResp.TotalCount, ShouldEqual, 0)
-				So(listResp.Result, ShouldHaveLength, 0)
-
-				validator.returnIsAdmin = true
-				listResp, err = api.List(ctx, &pb.ListNetworkServerRequest{
-					Limit:  10,
-					Offset: 0,
-				})
-				So(err, ShouldBeNil)
-				So(listResp.TotalCount, ShouldEqual, 1)
-				So(listResp.Result, ShouldHaveLength, 1)
-				So(listResp.Result[0].Name, ShouldEqual, "test ns")
-				So(listResp.Result[0].Server, ShouldEqual, "test-ns:1234")
-			})
-
-			Convey("Given a second organization and service-profile assigned to the first organization", func() {
-				org2 := storage.Organization{
-					Name: "test-org-2",
-				}
-				So(storage.CreateOrganization(storage.DB(), &org2), ShouldBeNil)
-
-				sp := storage.ServiceProfile{
-					NetworkServerID: resp.Id,
-					OrganizationID:  org.ID,
-					Name:            "test-sp",
-				}
-				So(storage.CreateServiceProfile(storage.DB(), &sp), ShouldBeNil)
-
-				Convey("Then List with organization id lists the network-servers for the given organization id", func() {
-					listResp, err := api.List(ctx, &pb.ListNetworkServerRequest{
-						Limit:          10,
-						OrganizationId: org.ID,
-					})
-					So(err, ShouldBeNil)
-					So(listResp.TotalCount, ShouldEqual, 1)
-					So(listResp.Result, ShouldHaveLength, 1)
-
-					listResp, err = api.List(ctx, &pb.ListNetworkServerRequest{
-						Limit:          10,
-						OrganizationId: org2.ID,
-					})
-					So(err, ShouldBeNil)
-					So(listResp.TotalCount, ShouldEqual, 0)
-					So(listResp.Result, ShouldHaveLength, 0)
-				})
-			})
+			assert.Equal(codes.NotFound, grpc.Code(err))
 		})
 	})
 }

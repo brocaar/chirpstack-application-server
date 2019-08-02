230c63da646855665e907120a32e82a9c0c5d657
Orne Brocaar <info@brocaar.com>
2019-08-02 08:50:09 -0600

    Remove unused import.

--- ui/src/views/devices/DeviceDetails.js
+++ ui/src/views/devices/DeviceDetails.js
@@ -5,7 +5,6 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";

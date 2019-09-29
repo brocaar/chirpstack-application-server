16e21655bba15bc1ddf5bccfc2d35a76ad8827d2
Orne Brocaar <info@brocaar.com>
2019-09-29 09:48:57 +0200

    Cleanup old freegeoip.net code.

--- ui/src/stores/LocationStore.js
+++ ui/src/stores/LocationStore.js
@@ -1,29 +1,5 @@
 import { EventEmitter } from "events";
 import "whatwg-fetch";
-import dispatcher from "../dispatcher";
-import history from "../history";
-
-
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    throw response.json();
-  }
-};
-
-function errorHandler(error) {
-  error.then((data) => {
-    if (data.code === 16) {
-      history.push("/login");
-    } else {
-      dispatcher.dispatch({
-        type: "CREATE_ERROR",
-        error: data,
-      });
-    }
-  });
-};
 
 
 class LocationStore extends EventEmitter {
@@ -31,26 +7,8 @@ class LocationStore extends EventEmitter {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         callbackFunc(position);
-      }, (error) => {
-        this.getGeoIPLocation(callbackFunc);
       });
-    } else {
-      this.getGeoIPLocation(callbackFunc);
     }
-  }
-
-  getGeoIPLocation(callbackFunc) {
-    fetch("https://freegeoip.net/json/")
-      .then(checkStatus)
-      .then((response) => response.json())
-      .then((responseData) => {
-        if(typeof(responseData.latitude) === "undefined") {
-          callbackFunc({coords: {latitude: 0, longitude: 0}});
-        } else {
-          callbackFunc({ coords: { latitude: responseData.latitude, longitude: responseData.longitude } });
-        }
-      })
-      .catch(errorHandler); 
   }
 }
 

b41c1521e0ea67812ff947efd9298fe0bae86df7
SAGAR PATEL <sagar.a.patel@slscorp.com>
2021-03-01 17:21:05 +0530

    Implement auto-migration schema of postgresql integration. (#573)
    Signed-off-by: SAGAR PATEL <sagar.a.patel@slscorp.com>
    Co-authored-by: Orne Brocaar <info@brocaar.com>

---
+++ internal/integration/postgresql/migrations/0001_initial.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS device_up;
+DROP TABLE IF EXISTS device_status;
+DROP TABLE IF EXISTS device_join;
+DROP TABLE IF EXISTS device_ack;
+DROP TABLE IF EXISTS device_error;
+DROP TABLE IF EXISTS device_location;
